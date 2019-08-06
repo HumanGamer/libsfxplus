04c57f72305bc22decd91d4bcdbd8f53e9c9d500
Human Gamer <humangamer.dev@hotmail.com>
2019-08-06 19:59:34 -0400

    Initialization, Shared Lib support, install CMake

--- include/sfxplus.h
+++ include/sfxplus.h
@@ -1,14 +1,29 @@
 #pragma once
 
-#define __SFXPLUSCALL __cdecl
-#define __SFXPLUSEXP extern
+#include "sfxplus_config.h"
+
+#define SFXPLUSCALL __cdecl
+
+#ifdef SFXPLUS_SHARED
+#ifdef SFXPLUS_SHARED_EXPORT
+#define SFXPLUSEXP extern __declspec(dllexport)
+#else
+#define SFXPLUSEXP extern __declspec(dllimport)
+#endif
+#else
+#define SFXPLUSEXP extern
+#endif
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-__SFXPLUSEXP bool __SFXPLUSCALL sfx_init(); // Initializes SFX+ Library
+/*
+ * Initializes SFX+ Library
+ * This must be the first thing called before any other functions in this library.
+ */
+SFXPLUSEXP bool SFXPLUSCALL sfx_init();
 
 #ifdef __cplusplus
 }
--- src/sfxplus.cpp
+++ src/sfxplus.cpp
@@ -1,7 +1,36 @@
 #include "sfxplus.h"
 
-bool __SFXPLUSCALL sfx_init()
+#include <sndfile.h>
+#include <al.h>
+#include <alc.h>
+
+bool sfx_initialized = false;
+
+ALCdevice* alc_device = nullptr;
+ALCcontext* alc_context = nullptr;
+
+bool SFXPLUSCALL sfx_init()
 {
-    // TODO: init library
+    if (sfx_initialized)
+        return true;
+
+    alc_device = alcOpenDevice(nullptr);
+    if (alc_device == nullptr)
+        return false;
+
+    ALCenum error;
+    error = alGetError();
+    if (error != AL_NO_ERROR)
+        return false;
+
+    alc_context = alcCreateContext(alc_device, nullptr);
+    if (!alcMakeContextCurrent(alc_context))
+        return false;
+
+    error = alGetError();
+    if (error != AL_NO_ERROR)
+        return false;
+    
+    sfx_initialized = true;
     return true;
 }
