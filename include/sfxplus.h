c8bad9a69f12dde2e86d0caff471bbced168d871
Human Gamer <humangamer.dev@hotmail.com>
2019-08-06 20:30:41 -0400

    Linux Support

--- include/sfxplus.h
+++ include/sfxplus.h
@@ -2,9 +2,34 @@
 
 #include "sfxplus_config.h"
 
-#define SFXPLUSCALL __cdecl
+#ifndef _WIN32
+#define __cdecl __attribute__((__cdecl__))
+#endif
 
-#ifdef SFXPLUS_SHARED
+// Check windows
+#if _WIN32 || _WIN64
+#if _WIN64
+#define SFXPLUS64
+#else
+#define SFXPLUS32
+#endif
+#elif __GNUC__ // Check GCC
+#if __x86_64__ || __ppc64__
+#define SFXPLUS64
+#else
+#define SFXPLUS32
+#endif
+#else // Fail and show an error
+#error "Missing feature-test macro for 32/64-bit on this compiler."
+#endif
+
+#ifdef SFXPLUS32
+#define SFXPLUSCALL __cdecl
+#else
+#define SFXPLUSCALL
+#endif
+
+#if defined(SFXPLUS_SHARED) && defined(_WIN32)
 #ifdef SFXPLUS_SHARED_EXPORT
 #define SFXPLUSEXP extern __declspec(dllexport)
 #else
