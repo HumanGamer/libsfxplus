36de7ccb663600efa37c0e4d279547997133ce07
Human Gamer <humangamer.dev@hotmail.com>
2019-08-07 22:30:45 -0400

    Multi-Threaded Audio Streaming

--- src/stream.cpp
+++ src/stream.cpp
@@ -1,16 +1,57 @@
 #include "core.h"
 
+#include <thread>
+#include <mutex>
 #include <vector>
 #include <cstring>
 #include <sndfile.h>
 #include <al.h>
 
-void SFXPLUSCALL sfx_source_open_stream(SFX_SOURCE source, const char* path)
+std::mutex sfx_stream_io_mutex, sfx_stream_play_mutex;
+std::vector<unsigned short> sfx_stream_data;
+size_t current;
+
+void sfx_add_data_internal(short* first, short* last)
 {
-    sfx_last_error = SFX_NO_ERROR;
+    std::lock(sfx_stream_io_mutex, sfx_stream_play_mutex);
+    sfx_stream_data.insert(sfx_stream_data.end(), first, last);
 
-    int bufCount = 3;
+    sfx_stream_io_mutex.unlock();
+    sfx_stream_play_mutex.unlock();
+}
 
+void sfx_buffer_data_internal(ALuint buf, ALenum format, ALsizei freq)
+{
+    std::lock(sfx_stream_play_mutex, sfx_stream_io_mutex);
+    unsigned short* buffer = (&sfx_stream_data.front() + current);
+
+    size_t size = sfx_stream_data.size() - current;
+    if (size > 4096)
+        size = 4096;
+
+    current += size;
+
+    alBufferData(buf, format,
+        buffer, size * sizeof(unsigned short), freq);
+
+    sfx_stream_play_mutex.unlock();
+    sfx_stream_io_mutex.unlock();
+}
+
+void sfx_run_stream_io_internal(SNDFILE* snd)
+{
+    sfx_stream_data.clear();
+
+    short read_buf[4096];
+
+    size_t read_size = 0;
+    while ((read_size = sf_read_short(snd, read_buf, 4096)) != 0)
+        sfx_add_data_internal(read_buf, read_buf + read_size);
+}
+
+void sfx_run_stream_openal_internal(SFX_SOURCE source, int bufCount, SF_INFO sfinfo)
+{
+    current = 0;
     ALuint* buffers = (ALuint*)malloc(bufCount * sizeof(ALuint));
     alGenBuffers((ALuint)bufCount, buffers);
     if (!sfx_checkerror_internal())
@@ -19,30 +60,14 @@ void SFXPLUSCALL sfx_source_open_stream(SFX_SOURCE source, const char* path)
         return;
     }
 
-    SF_INFO sfinfo;
-    memset(&sfinfo, 0, sizeof(sfinfo));
-
-    SNDFILE* snd = sf_open(path, SFM_READ, &sfinfo);
-    if (snd == nullptr)
-    {
-        sfx_last_error = SFX_FAIL_READ_FILE;
-        return;
-    }
-
-    short read_buf[4096];
-
-    size_t read_size = 0;
-    if ((read_size = sf_read_short(snd, read_buf, 4096)) == 0)
-        return;
-
     for (int i = 0; i < bufCount; i++)
     {
         if (buffers == nullptr) // This check is here to prevent warning C6011
             break;
 
-        alBufferData(buffers[i],
+        sfx_buffer_data_internal(buffers[i],
             sfinfo.channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16,
-            read_buf, read_size * sizeof(unsigned short), sfinfo.samplerate);
+            sfinfo.samplerate);
 
         if (!sfx_checkerror_internal())
         {
@@ -87,22 +112,21 @@ void SFXPLUSCALL sfx_source_open_stream(SFX_SOURCE source, const char* path)
             }
             numProcessed--;
 
-            if ((read_size = sf_read_short(snd, read_buf, 4096)) != 0)
-            {
-                alBufferData(buf, sfinfo.channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16,
-                    read_buf, read_size * sizeof(unsigned short), sfinfo.samplerate);
-                if (!sfx_checkerror_internal())
-                {
-                    sfx_last_error = SFX_FAIL_FILL_BUFFER;
-                    return;
-                }
+            sfx_buffer_data_internal(buf,
+                sfinfo.channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16,
+                sfinfo.samplerate);
 
-                alSourceQueueBuffers(source, 1, &buf);
-                if (!sfx_checkerror_internal())
-                {
-                    sfx_last_error = SFX_FAIL_QUEUE_BUFFER;
-                    return;
-                }
+            if (!sfx_checkerror_internal())
+            {
+                sfx_last_error = SFX_FAIL_FILL_BUFFER;
+                return;
+            }
+
+            alSourceQueueBuffers(source, 1, &buf);
+            if (!sfx_checkerror_internal())
+            {
+                sfx_last_error = SFX_FAIL_QUEUE_BUFFER;
+                return;
             }
         }
 
@@ -113,7 +137,7 @@ void SFXPLUSCALL sfx_source_open_stream(SFX_SOURCE source, const char* path)
             sfx_last_error = SFX_FAIL_GET_STATE;
             return;
         }
-            
+
         if (state != AL_PLAYING && state != AL_PAUSED)
         {
             int queued;
@@ -136,4 +160,33 @@ void SFXPLUSCALL sfx_source_open_stream(SFX_SOURCE source, const char* path)
     }
 
     alDeleteBuffers(bufCount, buffers);
+    if (!sfx_checkerror_internal())
+    {
+        sfx_last_error = SFX_FAIL_PLAY_SOURCE;
+        return;
+    }
+}
+
+void SFXPLUSCALL sfx_source_open_stream(SFX_SOURCE source, const char* path)
+{
+    sfx_last_error = SFX_NO_ERROR;
+
+    int bufCount = 6;
+
+    SF_INFO sfinfo;
+    memset(&sfinfo, 0, sizeof(sfinfo));
+
+    SNDFILE* snd = sf_open(path, SFM_READ, &sfinfo);
+    if (snd == nullptr)
+    {
+        sfx_last_error = SFX_FAIL_READ_FILE;
+        return;
+    }
+
+    std::thread io_thread(sfx_run_stream_io_internal, snd);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::thread openal_thread(sfx_run_stream_openal_internal, source, bufCount, sfinfo);
+
+    io_thread.join();
+    openal_thread.join();
 }
